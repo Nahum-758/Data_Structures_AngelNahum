0805faa005a8225ab86014450952ca5aab9c5b69
Nahum-758 <1225100758.ancs@gmail.com>
2026-09-18 15:00:58 -0600

    Repasando arreglos

---
+++ mx/edu/utng/arreglos2/RepasoArreglos2.java
@@ -0,0 +1,23 @@
+package mx.edu.utng;
+public class RepasoArreglos2 {
+
+	public static void main(String[] args) {
+		String[] nombres[] = new String[7][3];
+		
+		nombres[0][0] = "Jonathan Venancio";
+		nombres[0][1] = "Aguilar Hernandez";
+		nombres[0][2] = "18";
+
+		
+		nombres[1][0] = "Jose Emmanuel";
+		nombres[1][1] = "Arredondo Escalante";
+		nombres[1][2] = "22";
+
+		nombres[2][0] = "Yaneli";
+		nombres[2][1] = "Arredondo";
+		nombres[2][2] = "22";|
+
+
+	}
+
+}
