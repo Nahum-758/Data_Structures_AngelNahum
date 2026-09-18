929f271f2dee68d1040910113e6f7536e0d744d1
Nahum-758 <1225100758.ancs@gmail.com>
2026-09-18 14:05:31 -0600

    Repasando arreglos

--- mx/edu/utng/RepasoArreglos.java
+++ mx/edu/utng/RepasoArreglos.java
@@ -4,6 +4,11 @@ public class RepasoArreglos {
 	public static void main(String[] args) {
 		System.out.println("Manejando Arreglos");
 
+		String[] nombres = new String[10];
+		int edades[] = {20, 21, 22, 23};
+		System.out.println("Tamano" + nombres.length);
+		System.out.println("Edades" + edades.length);
+
 	}
 
 }
